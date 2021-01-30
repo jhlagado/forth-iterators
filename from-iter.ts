79228389f708925c80d0b65e50b9f6fc97d1976e
John Hardy <john.hardy@workjam.com>
2021-01-30 15:04:12 +1100

    save

--- from-iter.ts
+++ from-iter.ts
@@ -87,35 +87,3 @@ const iterator = [10, 20, 30, 40][Symbol.iterator]();
 const source = fromIter(iterator);
 
 forEach((x) => console.log(x))(source);
-
-///////////////////////////////////////////////////////////////
-
-// const fromIter = (iterator) => (start, sink) => {
-//   if (start !== 0) return;
-//   let inloop = false;
-//   let got1 = false;
-//   let completed = false;
-//   let res;
-//   function loop() {
-//     inloop = true;
-//     while (got1 && !completed) {
-//       got1 = false;
-//       res = iterator.next();
-//       if (res.done) {
-//         sink(2);
-//         break;
-//       } else sink(1, res.value);
-//     }
-//     inloop = false;
-//   }
-//   sink(0, (t) => {
-//     if (completed) return;
-
-//     if (t === 1) {
-//       got1 = true;
-//       if (!inloop && !(res && res.done)) loop();
-//     } else if (t === 2) {
-//       completed = true;
-//     }
-//   });
-// };
